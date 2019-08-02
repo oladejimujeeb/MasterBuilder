f9c48b6f3d71a413054ebf301c9979dbf63a62c0
maryam-olabisi <m.olabisimurit@gmail.com>
2019-08-02 14:48:59 +0100

    Jinja Templates Added

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask("__name__")
+app.config.from_pyfile('config.cfg')
+#db.init_app(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/land-info')
+def landInfo():
+    return render_template('land-info.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
