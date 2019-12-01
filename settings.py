81b4565dfd637c992582bbf45b6439223888c161
maryam-olabisi <m.olabisimurit@gmail.com>
2019-12-01 01:31:26 +0100

    config

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environment.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SECRET_KEY = os.environment.get('SECRET_KEY')
